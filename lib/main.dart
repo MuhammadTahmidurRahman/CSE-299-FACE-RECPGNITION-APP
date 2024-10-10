bdf19ba8f59b6ed306455c0e45162f00bc48679e
Anika-Tabassum23 <anika.enan@gmail.com>
2024-10-10 10:56:08 +0600

    Update main.dart
    Updates main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,14 @@
 import 'package:flutter/material.dart';
-import 'splash.dart';  // Import your splash screen
-import 'package:firebase_core/firebase_core.dart';  // Import Firebase core
-
-void main() async {
-  // Ensure that widget binding is initialized
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize Firebase
-  await Firebase.initializeApp();  // This is crucial for Firebase to work properly
-
-  // Run the main application
+import 'splash.dart';  // Import the SplashScreen file
+void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),  // Your splash screen
+      debugShowCheckedModeBanner: false, // Hides the debug banner
+      home: SplashScreen(), // Set SplashScreen as the initial screen
     );
   }
 }
