444852b28d22219634ebb525c786f7fbfa53fbc8
Anika-Tabassum23 <anika.enan@gmail.com>
2024-10-10 11:01:24 +0600

    all Change

---
+++ lib/forgot_password.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class ForgotPasswordPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forgot Password'),
+      ),
+      body: Center(
+        child: Text('Forgot Password Page'),
+      ),
+    );
+  }
+}
