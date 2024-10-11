53097997ae80c93a2abe269696d76921f268e57c
Muhammad Tahmidur Rahman <muhammad.rahman12@northsouth.edu>
2024-10-11 18:03:56 +0600

    Storage added and image upload to the storage is successfull.

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'signup.dart'; // Import the signup page
 import 'login.dart';  // Import the login page
 
