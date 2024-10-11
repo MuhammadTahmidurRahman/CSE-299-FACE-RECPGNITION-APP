ad14c86a3ef2a649863aaa8fdfa56c6037b2bfb7
Mohosina Islam Disha <mohosina.disha@northsouth.edu>
2024-10-11 17:06:21 +0600

    Changed login File

--- lib/login.dart
+++ lib/login.dart
@@ -9,6 +9,27 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool _obscureText = true;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();  // Key for form validation
+  String? _errorMessage;
+
+  // Mock authentication method (Replace with actual implementation)
+  Future<bool> _authenticateUser(String email, String password) async {
+    // Simulate a call to the backend to check the credentials
+    if (email == "test@example.com" && password == "password123") {
+      return true;  // Credentials match
+    } else if (email != "test@example.com") {
+      setState(() {
+        _errorMessage = "User does not exist. Please check your email.";
+      });
+    } else {
+      setState(() {
+        _errorMessage = "Incorrect password. Please try again.";
+      });
+    }
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,115 +42,149 @@ class _LoginPageState extends State<LoginPage> {
             fit: BoxFit.cover,
           ),
           SafeArea(
-            child: SingleChildScrollView(  // Make the content scrollable when keyboard appears
+            child: SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Back arrow icon
-                    IconButton(
-                      icon: Icon(Icons.arrow_back, color: Colors.white),
-                      onPressed: () {
-                        Navigator.pop(context);  // Navigate back to Welcome Page
-                      },
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Log in to PicTora',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Enter the email you have registered with.',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    SizedBox(height: 40),
-                    // Email field
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        labelStyle: TextStyle(color: Colors.white),
-                        filled: true,
-                        fillColor: Colors.black.withOpacity(0.3),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 20),
-                    // Password field
-                    TextField(
-                      obscureText: _obscureText,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        labelStyle: TextStyle(color: Colors.white),
-                        filled: true,
-                        fillColor: Colors.black.withOpacity(0.3),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _obscureText ? Icons.visibility_off : Icons.visibility,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _obscureText = !_obscureText;  // Toggle password visibility
-                            });
-                          },
-                        ),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(height: 10),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
+                child: Form(
+                  key: _formKey,  // Wrap the form with a validation key
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Back arrow icon
+                      IconButton(
+                        icon: Icon(Icons.arrow_back, color: Colors.white),
                         onPressed: () {
-                          // Navigate to Forgot Password page
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => ForgotPasswordPage()),
-                          );
+                          Navigator.pop(context);  // Navigate back to Welcome Page
                         },
-                        child: Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Log in to PicTora',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    // Log in Button
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigate to home page after login
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => HomePage()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        padding: EdgeInsets.symmetric(vertical: 15),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
+                      SizedBox(height: 10),
+                      Text(
+                        'Enter the email you have registered with.',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                      SizedBox(height: 40),
+                      // Email field
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          labelStyle: TextStyle(color: Colors.white),
+                          filled: true,
+                          fillColor: Colors.black.withOpacity(0.3),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      // Password field
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: _obscureText,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.white),
+                          filled: true,
+                          fillColor: Colors.black.withOpacity(0.3),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscureText ? Icons.visibility_off : Icons.visibility,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _obscureText = !_obscureText;  // Toggle password visibility
+                              });
+                            },
+                          ),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {
+                            // Navigate to Forgot Password page
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => ForgotPasswordPage()),
+                            );
+                          },
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(color: Colors.white),
+                          ),
                         ),
                       ),
-                      child: Center(
-                        child: Text(
-                          'Log in',
-                          style: TextStyle(color: Colors.white, fontSize: 16),
+                      SizedBox(height: 20),
+                      if (_errorMessage != null)  // Display error message if any
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10.0),
+                          child: Text(
+                            _errorMessage!,
+                            style: TextStyle(color: Colors.red, fontSize: 14),
+                          ),
+                        ),
+                      // Log in Button
+                      ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            // Validate email and password fields
+                            bool isAuthenticated = await _authenticateUser(
+                              _emailController.text,
+                              _passwordController.text,
+                            );
+                            if (isAuthenticated) {
+                              // Navigate to home page if authentication is successful
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(builder: (context) => HomePage()),
+                              );
+                            }
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Log in',
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
